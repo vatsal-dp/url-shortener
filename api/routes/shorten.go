9187f579035833f2a3664e4db5fd3389a35d1052
vatsal-dp <ptelvtsl@gmail.com>
2024-12-28 15:08:40 -0500

    rate limit implemented

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,9 +1,14 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
+	"github.com/vatsal-dp/url-shortener/database"
+	"github.com/vatsal-dp/url-shortener/helpers"
 )
 
 type request struct {
@@ -31,6 +36,24 @@ func ShortenURL(c *fiber.Ctx) error {
 	}
 
 	//implement rate limiting
+
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+
+	val, err := r2.Get(database.Ctx, c.IP()).Result()
+	if err == redis.Nil {
+		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else {
+		val, _ = r2.Get(database.Ctx, c.IP()).Result()
+		valInt, _ := strconv.Atoi(val)
+		if valInt <= 0 {
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+				"error":            "rate limit exceeded",
+				"rate_limit_reset": limit / time.Nanosecond / time.Minute,
+			})
+		}
+	}
 
 	//check if input is an actual URL
 
