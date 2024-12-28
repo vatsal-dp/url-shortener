655fca9517d1ba969ecf12344fb4578179d23c8a
vatsal-dp <ptelvtsl@gmail.com>
2024-12-28 02:45:53 -0500

    basic edge case of url handled

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,1 +1,54 @@
 package routes
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber"
+)
+
+type request struct {
+	URL         string        `json:"url"`
+	CustomShort string        `json:"short"`
+	Expiry      time.Duration `json:"expiry"`
+}
+
+type response struct {
+	URL             string        `json:"url"`
+	CustomShort     string        `json:"short"`
+	Expiry          time.Duration `json:"expiry"`
+	XRateRemaining  int           `json:"rate_limit"`
+	XRateLimitReset time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse JSON",
+		})
+	}
+
+	//implement rate limiting
+
+	//check if input is an actual URL
+
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "invalid URL",
+		})
+	}
+
+	//check for domain error
+
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+			"error": "you cant hack me :)",
+		})
+	}
+
+	//enforce https, SSL
+
+	body.URL = helpers.EnforceHTTPS(body.URL)
+}
